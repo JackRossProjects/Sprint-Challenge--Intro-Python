e38d5d0010d1ce286f2a204e7ff3390f0e4d921b
User <jackrosscoding@gmail.com>
2020-07-10 09:43:06 -0600

    oop1 test passed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -7,6 +7,12 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
+
+''' 
+class DerivedClassName(BaseClassName):
+    pass
+'''
+
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
@@ -17,3 +23,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+''' The base class is 'vehicle' '''
+
+class Vehicle:
+    # This is the base class which other classes will inherit from
+    def __init__(self):
+        def descrition(self):
+            print("I'm a vehicle")
+
+class FlightVehicle(Vehicle):
+    pass        
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+vehicle = Vehicle()
+ground_vehicle = GroundVehicle()
+flight_vehicle = FlightVehicle()
+airplane = Airplane()
+starship = Starship()
+car = Car()
+motorcycle = Motorcycle()
